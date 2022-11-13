38f53a0852500f02f3f5fc7aaf32505d0a3cf089
MarkEmdin <emdinmark@gmail.com>
2022-11-13 22:20:14 +0200

    Removing unused files from git

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const logger = require("./logger");
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user:"postgres",
-    password:'root',
-    host:'localhost',
-    port:"5433",
+    user:"masterUsername",
+    password:'root2749405',
+    host:'rds-postgresql-mybot.cnotjgtzcbve.eu-central-1.rds.amazonaws.com',
+    port:"5432",
     database:"postgres"
 })
 pool.connect(err => {
@@ -15,3 +15,7 @@ pool.connect(err => {
     }
 })
 module.exports = pool
+
+// user:"postgres",
+//     password:'root',
+//     host:'localhost',
